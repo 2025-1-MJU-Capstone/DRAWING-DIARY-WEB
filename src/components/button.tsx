bf8dfb546741ea39abf21e67f5b4299c6565f119
Choi JunHo <duekzim@gmail.com>
2025-05-02 14:33:31 +0900

    ✨ feat: button 컴포넌트 생성

---
+++ src/components/button.tsx
@@ -0,0 +1,62 @@
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  ImageSourcePropType,
+} from "react-native";
+
+interface ButtonProps {
+  theme: "primary" | "secondary";
+  onPress?: () => void;
+  label: string;
+  backgroundColor: string;
+  icon?: ImageSourcePropType;
+}
+
+export default function Button({
+  theme,
+  onPress,
+  label,
+  backgroundColor,
+  icon,
+}: ButtonProps) {
+  const buttonStyle =
+    theme === "primary" ? styles.primaryContainer : styles.secondaryContainer;
+  return (
+    <Pressable onPress={onPress} style={[buttonStyle, { backgroundColor }]}>
+      {icon && (
+        <Image
+          style={{ width: 18, height: 18, marginRight: 5 }}
+          source={icon}
+        />
+      )}
+      <Text style={styles.label}>{label}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  primaryContainer: {
+    width: 300,
+    height: 45,
+    borderRadius: 12,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  secondaryContainer: {
+    width: 125,
+    height: 45,
+    borderRadius: 6,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  label: {
+    color: "#4B4B4B",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    fontFamily: "PretendardVariable",
+  },
+});
