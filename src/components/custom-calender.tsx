6490762206afd83a29012cae53ea6229b62130e9
Choi JunHo <duekzim@gmail.com>
2025-05-14 00:24:00 +0900

    ♻️ refactor: shadow style 적용

--- src/components/custom-calender.tsx
+++ src/components/custom-calender.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Calendar } from "react-native-calendars";
 import useCalendar from "../business/use-calender.hooks";
 interface DateDataType {
@@ -13,15 +13,16 @@ export default function CustomCalender() {
   const { markingDates, selectingDay } = useCalendar();
 
   return (
-    <Calendar
-      //onMonthChange 속성 추가하기
-      markedDates={markingDates}
-      onDayPress={(dateData: DateDataType) => {
-        selectingDay(dateData.dateString);
-      }}
-      style={styles.container}
-      firstDay={1}
-    />
+    <View style={styles.container}>
+      <Calendar
+        //onMonthChange 속성 추가하기
+        markedDates={markingDates}
+        onDayPress={(dateData: DateDataType) => {
+          selectingDay(dateData.dateString);
+        }}
+        firstDay={1}
+      />
+    </View>
   );
 }
 
@@ -29,5 +30,13 @@ const styles = StyleSheet.create({
   container: {
     width: 306,
     height: 150,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
 });
