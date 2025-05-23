6a6dbf84e3a3b6942fd1ca220c30c613ff66176f
Choi JunHo <duekzim@gmail.com>
2025-05-23 23:23:54 +0900

    ♻️ refactor: expo env 이름 변경

--- src/utils/axios-instance.ts
+++ src/utils/axios-instance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const springInstance = axios.create({
-  baseURL: process.env.EXPO_SPRING_API_URL,
+  baseURL: process.env.EXPO_PUBLIC_SPRING_API_URL,
   timeout: 3000,
 });
