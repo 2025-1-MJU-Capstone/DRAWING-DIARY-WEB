30bded44b4fd0464aa6a73a3881f99af82576262
Choi JunHo <duekzim@gmail.com>
2025-05-25 17:54:20 +0900

    ✨ feat: 설정 화면 퍼블리싱

--- src/app/(app)/setting.tsx
+++ src/app/(app)/setting.tsx
@@ -1,4 +1,27 @@
-import { Text } from "react-native";
-export default function Setting() {
-  return <Text>Setting</Text>;
+import Button from "@/src/components/button";
+import CustomCalender from "@/src/components/custom-calender";
+import { useSession } from "@/src/stores/store/auth-context.store";
+import { router } from "expo-router";
+import { Text, View } from "react-native";
+export default function Index() {
+  const { signOut } = useSession();
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Button
+        theme='primary'
+        label='로그아웃하기'
+        backgroundColor='#B8E9C0'
+        onPress={() => signOut()}
+      />
+      <Button
+        theme='primary'
+        label='Intro가기'
+        backgroundColor='#FFECA5'
+        onPress={() => {
+          router.push("../(landing)/font-make");
+        }}
+      />
+    </View>
+  );
 }
